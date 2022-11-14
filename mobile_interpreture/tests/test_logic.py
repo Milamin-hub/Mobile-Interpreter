a127d2438a9a8253ca4f06fb45202b61fe6735d6
milamin <OsKalenik.com>
2022-11-14 11:41:15 +0200

    debug

--- mobile_interpreture/tests/test_logic.py
+++ mobile_interpreture/tests/test_logic.py
@@ -13,19 +13,3 @@ class TestIntepreture(unittest.TestCase):
     """ Test class Interpreture """
     def __init__(self) -> None:
         super().__init__()
-import unittest
-import os
-import sys
-
-
-try:
-    from mobile.logic import Interpreture
-except:
-    sys.path.insert(1, os.path.join(sys.path[0], '..'))
-    from mobile.logic import Interpreture
-
-
-class TestIntepreture(unittest.TestCase):
-    """ Test class Interpreture """
-    def __init__(self) -> None:
-        super().__init__()
