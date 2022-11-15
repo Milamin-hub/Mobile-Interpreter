9e19984be20a20779ca76a073c6cf10fa2c138f4
milamin <aminsasa187@gmail.com>
2022-11-15 12:24:28 +0200

    add tests(pytest) for logic

--- mobile_interpreture/tests/test_logic.py
+++ mobile_interpreture/tests/test_logic.py
@@ -1,4 +1,3 @@
-import unittest
 import os
 import sys
 
@@ -9,7 +8,29 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from mobile.logic import Interpreture
 
 
-class TestIntepreture(unittest.TestCase):
-    """ Test class Interpreture """
-    def __init__(self) -> None:
-        super().__init__()
+i = Interpreture()
+name = "line"
+txt = "print('hello world')"
+
+def test_create_folder():
+    assert "files" in os.listdir()
+
+def test_create_file():
+    i.create_file(name)
+    assert "%s.py" % name in os.listdir(path="files")
+
+def test_edit_file():
+    i.edit_file(txt, name)
+    with open('files/%s.py' % name, "r") as file:
+        assert txt == file.read()
+    i.del_file(name)
+    i.create_file(name)
+
+def test_read_file():
+    with open('files/%s.py' % name, "r") as file:
+        assert i.read_file(name) == file.read()
+
+def test_del_file():
+    i.del_file(name)
+    assert "%s.py" % name not in os.listdir(path="files")
+
