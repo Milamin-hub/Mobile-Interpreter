2a08de5f664b3b2528cb8cc336ab5ead2087d552
milamin <aminsasa187@gmail.com>
2022-11-23 00:39:45 +0200

    Added title current file

--- mobile_interpreture/mobile/md.py
+++ mobile_interpreture/mobile/md.py
@@ -1,11 +1,7 @@
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.filemanager import MDFileManager
-from kivymd.uix.tab import MDTabsBase
-from kivymd.uix.floatlayout import MDFloatLayout
-from plyer import filechooser
-from kivymd.toast import toast
-
 from kivymd.app import MDApp
+from io import StringIO
 
 import sys
 import os
@@ -26,15 +22,21 @@ class Container(MDBoxLayout):
 
     def select_path(self, path) -> str:
         try:
-            self.exit_manager()
-            file = open('%s' % path, "r").readlines()
-            text = ""
-            for i in file:
-                text += i
-            self.ids.fname.opacity = 1
-            self.ids.fname.enabled = True
-            self.ids.fname.text = text
-            self.path = path
+            p, file_ext = os.path.splitext(path)
+            if file_ext == ".py":
+                self.exit_manager()
+                file = open('%s' % path, "r").readlines()
+                text = ""
+                for i in file:
+                    text += i
+                self.ids.fname.opacity = 1
+                self.ids.fname.enabled = True
+                self.ids.fname.text = text
+                self.path = path
+                n = os.path.basename(self.path)
+                self.ids.filen.text = n
+            else:
+                pass
         except IsADirectoryError:
             pass
         
@@ -42,10 +44,27 @@ class Container(MDBoxLayout):
         self.file_manager.close()
 
     def save_file(self):
+        try:
+            name = os.path.basename(self.path)
+            with open('mobile_interpreture/assets/%s' % name, "w") as file:
+                file.write(self.ids.fname.text)
+        except IsADirectoryError as e:
+            print(e)
+        
+    def run_file(self):
         name = os.path.basename(self.path)
-        with open('mobile_interpreture/assets/%s' % name, "w") as file:
-            file.write(self.ids.fname.text)
-    
+        try:
+            with open('mobile_interpreture/assets/%s' % name, "r") as file:
+                text = ""
+                for i in file:
+                    text += i
+                old_stdout = sys.stdout
+                redirected_output = sys.stdout = StringIO()
+                exec(text)
+                sys.stdout = old_stdout
+                self.ids.run.text = redirected_output.getvalue().strip()
+        except Exception as e:
+            print(e)
         
 
 class UIApp(MDApp):
